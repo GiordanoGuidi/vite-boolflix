78ee9f019625b66fd7de1bfe74dd446a4670f1e6
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-02-07 16:18:01 +0100

    scaffolding added

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default {
+    name: 'AppHeader'
+}
+
+</script>
+
+<template>
+    <header>
+        <h1>BOOLFIX</h1>
+
+
+    </header>
+</template>
+
+<style>
+header {
+    height: 150px;
+    background-color: black;
+    width: 100%;
+}
+</style>
