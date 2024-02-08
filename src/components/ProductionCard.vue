47a4565db4c6429a70bb6f2219ae9a39df675804
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-02-08 17:31:16 +0100

    refactoring in ProductionCard component

--- src/components/ProductionCard.vue
+++ src/components/ProductionCard.vue
@@ -8,13 +8,6 @@ export default {
     }),
     props: { seriesList: Array, production: Object },
     methods: {
-        getLanguage(language) {
-            if (language === 'en' || language === 'it') {
-                return
-            } else {
-                return language
-            }
-        },
         /*FUNZIONE CHE CREA URL SE NELL'API C'E' L'IMMAGINE
         ALTRIMENTI METTE UN IMMAGINE DEFAULT*/
         getUrlImage(production) {
@@ -26,7 +19,6 @@ export default {
                 return defaultImage
             }
         },
-
     },
 
     computed: {
@@ -35,13 +27,28 @@ export default {
             const vote = this.production.vote_average;
             const voteRounded = Math.round(vote / 2)
             return voteRounded
+        },
+        //LINGUA ORIGINALE 
+        lang() {
+            return this.production.original_language
 
+        },
+
+        //CONTROLLIAMO SE FLAG INCLUDE LE LINGUE DELLE BANDIERE
+        hasFlag() {
+            const flags = ['it', 'en']
+            return flags.includes(this.production.original_language);
+        },
+
+        // GENERATO URL COMPLETO DINAMICAMENTE
+        flagSrc() {
+            const url = new URL(`../assets/img/${this.lang}.png`, import.meta.url)
+            return url.href
         }
     }
 
 
 }
-
 </script>
 
 <template>
@@ -50,30 +57,18 @@ export default {
         <li>
             <p>{{ production.title || production.name }}</p>
             <p>{{ production.original_title || production.original_name }}</p>
-            <div :class="production.original_language">{{ getLanguage(production.original_language) }}</div>
+            <img class="flag" v-if="hasFlag" :src="flagSrc" :alt="lang">
+            <span v-else>{{ lang }}</span>
             <p>{{ this.voteRounded }}</p>
             <img :src="getUrlImage(production)" :alt="production.title">
         </li>
     </ul>
 </template>
 
-<style>
-.en {
-    background-image: url(../assets/img/en.png);
-    background-position: center;
-    background-size: cover;
-    margin-bottom: 10px;
-    height: 30px;
+<style scoped>
+img.flag {
     width: 50px;
-}
-
-.it {
-    background-image: url(../assets/img/it.png);
-    background-position: center;
-    background-size: cover;
     height: 30px;
-    width: 50px;
     margin-bottom: 10px;
-
 }
 </style>
